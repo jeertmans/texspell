9c1ac6f3a8a6d4574fb89e1802e549db91c172f1
Louis Devillez <louis.devillez@gmail.com>
2021-01-22 17:29:08 +0100

    Better way than .diff (and remove -U)

--- README.md
+++ README.md
@@ -23,11 +23,11 @@ Optionnal:
 ### Generating diff files
 
 * [x] have a working spell check that generate .diff files ?
-* [ ] maybe find a better representation than .diff files ?
-* [ ] add line number
-* [ ] Propose multiple correction
-* [ ] Diff reporting
-* [ ] Do not generate empty files
+* [x] maybe find a better representation than .diff files ?
+* [x] add line number
+* [x] Propose multiple correction
+* [x] Diff reporting
+* [x] Do not generate empty files
 
 ### Command line tools
 
@@ -36,8 +36,8 @@ Optionnal:
 * [x] Add installation instruction
 * [x] Add completion on command line
 * [x] Add help
-* [ ] Make hunspell shut up
-* [ ] add verbose environment
+* [x] Make hunspell shut up
+* [x] add verbose environment
 * [ ] Use shasum to only check files that changed (optional)
 * [ ] Do not explore hidden directories (only if specified)
 
--- spellcheck.sh
+++ spellcheck.sh
@@ -12,6 +12,8 @@
 ignore="! -iname colors.tex"
 CLEAN=0
 SPELL=1
+VERBOSE=1
+REPORT=1
 
 # Flags handler
 while test $# -gt 0; do
@@ -23,6 +25,7 @@ while test $# -gt 0; do
       echo "-h, --help: get this help"
       echo "-c, --clean: Remove all .diff in the . directiory and sub-directories"
       echo "-o, --clean-only: Do not generate the .diff"
+      echo "--no-report: Do not produce a report"
       echo "-v, --version: get version number"
       echo " "
       exit 0
@@ -40,6 +43,10 @@ while test $# -gt 0; do
       SPELL=0
       shift
       ;;
+    --no-report)
+      REPORT=0
+      shift
+      ;;
     *)
       echo "Unknown flag: "${1}
       exit 1
@@ -55,19 +62,75 @@ fi
 # Typechecking
 if [ $SPELL -eq 1 ]; then
   # Generate diff files
+  
+  FILES=0
+  EMPTY_FILES=0
+  ERRORS=0
+  UNKNOWN_WORDS=0
+  if [ $REPORT -eq 1 ]; then 
+    rm report_texspell
+    touch report_texspell
+    echo "Start " $(date) > report_texspell 
+    echo "" >> report_texspell
+  fi
+
   for file in $(find . -type f -iname "*.tex" $ignore); do
-    echo $file
-    hunspell -t -U -i utf-8 $file > $file.temp 
-    diff $file $file.temp > $file.diff
-    rm $file.temp
-    if [ -s $file.diff ]; then
-      echo "ok" 
-    else
-      echo $file " is empty"
-      rm $file.diff
+    if [ $VERBOSE -ge 1 ]; then
+      echo $file
     fi
+
+    hunspell -a -t -i utf-8 -d en_US <$file | grep '[\#\&]' > $file.tmp
+    sed -i '1d' $file.tmp
+    hunspell -L -i utf-8 -d en_US <$file > $file.tmp2
+    NLINES=$(wc -l $file.tmp | awk '{ print $1 }') 
+
+    if [ $NLINES -eq 0 ]; then
+      if [ $VERBOSE -ge 1 ]; then
+        echo $file " is empty"
+      fi
+
+      if [ $REPORT -eq 1 ]; then
+        echo $file " no errors" >> report_texspell
+        EMPTY_FILES=$(($EMPTY_FILES+1))
+      fi
+    else
+      #Erase file
+      echo "Created by texspell">$file.diff
+
+      for (( i=1; i <= $NLINES ; i++ ))
+      do
+        echo "----" >> $file.diff
+        echo $(sed "${i}q;d" $file.tmp2 | grep -Fx -n -f - $file | cut -f1 -d:) >> $file.diff
+        echo $(sed "${i}q;d" $file.tmp2) >> $file.diff
+        echo $(sed "${i}q;d" $file.tmp) >> $file.diff
+      done
+
+      if [ $REPORT -eq 1 ]; then
+        echo $file " Errors: " $(grep -c '[\&]' $file.tmp) "Unkown words" $(grep -c '[\#]' $file.tmp) >> report_texspell
+        ERRORS=$(($ERRORS +  $(grep -c '[\&]' $file.tmp)))
+        UNKNOWN_WORDS=$(($UNKNOWN_WORDS + $(grep -c '[\#]' $file.tmp)))
+        FILES=$(($FILES+1))
+      fi
+    fi
+
+
+    rm $file.tmp
+    rm $file.tmp2
+
   done
-  #find . -type f -iname "*.tex" $ignore | xargs -n 1 -t -I % sh -c 'hunspell -t -U -i utf-8 % > %.temp; diff % %.temp > %.diff; rm %.temp'
+  if [ $REPORT -eq 1 ]; then
+    sed -i "1 aEnd $(date)"  report_texspell
+    echo "" >> report_texspell
+    echo "Errors files " $FILES >> report_texspell
+    echo "Empty files: " $EMPTY_FILES >> report_texspell
+    echo "Errors: " $ERRORS >> report_texspell
+    echo "Unknown words: " $UNKNOWN_WORDS >> report_texspell
+    if [ $VERBOSE -ge 1 ]; then
+      echo ""
+      echo ""
+      cat report_texspell
+    fi
+  fi
 fi
 
 
