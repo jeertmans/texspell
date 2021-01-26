9883eb2c52faf6c39f0db3e2d1814ece107900ec
Louis Devillez <louis.devillez@gmail.com>
2021-01-26 13:01:34 +0100

    Hidden not by default and correct the script and better completion

--- texspell-complete.sh
+++ texspell-complete.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-complete -W "--clean --help --version" texspell
+
+_texspell_completion()
+{
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts="-o --clean-only --no-report -a --all -c --clean -h --help -v --version"
+
+  if [[ ${cur} == -* ]]; then
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+  fi
+
+  
+}
+#complete -F _texspell_completion texspell
+complete -o default -F _texspell_completion texspell
 
 
