f63f777dce061c974074834433d84e4d0425b2c4
Eertmans <jeertmans@icloud.com>
2021-01-27 12:25:09 +0100

    towards cleaner output and code (WIP)

--- spellcheck.sh
+++ spellcheck.sh
@@ -6,11 +6,25 @@
 
 # Colors
 # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
-RED='\033[0;31m'
-NC='\033[0m' # No Color
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+NC="\033[0m" # No Color
 
 DIFF_EXT=".diff" # Might be changed or choosed by user later... ?
 TEX_EXT=".tex"
+
+
+DOC="
+\ttexspell -- Command line spell-checker tool for TeX documents\n
+\n
+Options:\n
+-h, --help: get this help\n
+-a, --all: Clean/Check also hidden files and hidden directories\n
+-c, --clean: Remove all .diff in the . directiory and sub-directories\n
+-o, --clean-only: Do not generate the ${DIFF_EXT}\n
+--no-report: Do not produce a report\n
+-v, --version: get version number\n
+\n"
 
 ##################
 # Default values #
@@ -25,6 +39,7 @@ CHECK_HIDDEN=0 #Check or not (spell/clean) files in hidden dir/files
 SRC="." # SRC to check
 SRCISFILE=0 # Is SRC a file
 TEMP_DIR=""
+REPORT_FILE="report_texspell"
 
 #################
 # Flags handler #
@@ -32,16 +47,7 @@ TEMP_DIR=""
 while test $# -gt 0; do
   case "$1" in
     -h|--help)
-      echo "  texspell -- Command line spell-checker tool for TeX documents"
-      echo " "
-      echo "Options:"
-      echo "-h, --help: get this help"
-      echo "-a, --all: Clean/Check also hidden files and hidden directories"
-      echo "-c, --clean: Remove all .diff in the . directiory and sub-directories"
-      echo "-o, --clean-only: Do not generate the .diff"
-      echo "--no-report: Do not produce a report"
-      echo "-v, --version: get version number"
-      echo " "
+      echo -e $DOC
       exit 0
       ;;
     -v|--version)
@@ -128,6 +134,32 @@ function remove_colors {
   $(sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" $IN)
 }
 
+# Substitute regex pattern
+# 1 - Input
+# 2 - Search pattern, ex.: "([a-zA-Z])"
+# 3 - Sub. pattern, ex.: "_\1_" to add "_" around matching patterns
+#
+# R - The input with substituted patterns
+function regex_sub {
+  local STR=$1
+  local PAT=$2
+  local SUB=$3
+  echo $(sed -E "s/${PAT}/${SUB}/g" $STR)
+}
+
+# Substitute regex pattern in file (in-place)
+# 1 - Input
+# 2 - Search pattern, ex.: "([a-zA-Z])"
+# 3 - Sub. pattern, ex.: "_\1_" to add "_" around matching patterns
+#
+# R - Nothing
+function regex_sub_file {
+  local FILE=$1
+  local PAT=$2
+  local SUB=$3
+  sed -i -E "s/${PAT}/${SUB}/g" $FILE
+}
+
 ####################
 # Script execution #
 ####################
@@ -153,10 +185,10 @@ if [ $SPELL -eq 1 ]; then
   ERRORS=0
   UNKNOWN_WORDS=0
   if [ $REPORT -eq 1 ]; then 
-    rm report_texspell
-    touch report_texspell
-    echo "Start " $(date) > report_texspell 
-    echo "" >> report_texspell
+    rm $REPORT_FILE
+    touch $REPORT_FILE
+    echo "Start " $(date) > $REPORT_FILE 
+    echo "" >> $REPORT_FILE
   fi
   
   echo "Processing files..."
@@ -165,35 +197,41 @@ if [ $SPELL -eq 1 ]; then
 
   mapfile -d $'\0' FILES_LIST < <(find_files $SRC $TEX_EXT $CHECK_HIDDEN $ignore)
 
-  for file in ${FILES_LIST[@]}; do
+  for FILE in ${FILES_LIST[@]}; do
     if [ $VERBOSE -ge 1 ]; then
       echo $file
     fi
 
-    filename=$(basename $file)
-    TMP_FILE_1=$(mktemp -p "${TEMP_DIR}" "${filename}1_${date}_XXXXX.tmp")
-    TMP_FILE_2=$(mktemp -p "${TEMP_DIR}" "${filename}2_${date}_XXXXX.tmp")
+    FILENAME=$(basename $FILE)
+    TMP_FILE_1=$(mktemp -p "${TEMP_DIR}" "${FILENAME}1_${date}_XXXXX.tmp")
+    TMP_FILE_2=$(mktemp -p "${TEMP_DIR}" "${FILENAME}2_${date}_XXXXX.tmp")
 
-    hunspell -a -t -i utf-8 -d en_US <$file | grep -v '[\*]' > $TMP_FILE_1
+    hunspell -a -t -i utf-8 -d en_US <$FILE | grep -v '[\*]' > $TMP_FILE_1
+    # Whe need to clean the first line of the file containing a header from Ispell
     sed -i '1d' $TMP_FILE_1
     sed -i '/^$/N;/^\n$/D' $TMP_FILE_1
-    hunspell -L -t -i utf-8 -d en_US <$file > $TMP_FILE_2
+    # Reformat spelling suggestion with colors:
+    # L{lineno}: {error} => ({# of sugg.}) {suggestions}
+    regex_sub_file $TMP_FILE_1 "&\s(\S+)\s([0-9]+)\s([0-9]+):\s(.+)" "L\3: \\${RED}\1\\${NC} => (\2) \\${GREEN}\4\\${NC}\n"
+    hunspell -L -t -i utf-8 -d en_US <$FILE > $TMP_FILE_2
     NLINES=$(wc -l $TMP_FILE_1 | awk '{ print $1 }') 
 
     if [ $NLINES -eq 0 ]; then
       if [ $VERBOSE -ge 1 ]; then
-        echo $file " is empty"
+        echo $FILE " is empty"
       fi
 
       if [ $REPORT -eq 1 ]; then
-        echo $file " no errors" >> report_texspell
+        echo $file " no errors" >> $REPORT_FILE
         EMPTY_FILES=$(($EMPTY_FILES+1))
       fi
     else
       #Erase file
-      cat $TMP_FILE_1
+      echo -e $(cat $TMP_FILE_1)
       cat $TMP_FILE_2
-      echo "Created by texspell">$file.diff
+
+      DIFF_FILE="${FILE}${DIFF_EXT}"
+      echo "Created by texspell">$DIFF_FILE
 
       j=0
       for (( i=1; i <= $NLINES ; i++ ))
@@ -201,15 +239,15 @@ if [ $SPELL -eq 1 ]; then
         if [ -z "$(sed "${i}q;d" $TMP_FILE_1)" ]; then
           j=$((j+1))
         else
-          echo "----" >> $file.diff
-          echo $(sed "${j}q;d" $TMP_FILE_2 | grep -Fx -n -f - $file | cut -f1 -d:) >> $file.diff
-          echo $(sed "${j}q;d" $TMP_FILE_2) >> $file.diff
-          echo $(sed "${i}q;d" $TMP_FILE_1) >> $file.diff
+          echo "----" >> $DIFF_FILE
+          echo -e $(sed "${j}q;d" $TMP_FILE_2 | grep -Fx -n -f - $FILE | cut -f1 -d:) >> $DIFF_FILE
+          echo -e $(sed "${j}q;d" $TMP_FILE_2) >> $DIFF_FILE
+          echo -e $(sed "${i}q;d" $TMP_FILE_1) >> $DIFF_FILE
         fi
       done
 
       if [ $REPORT -eq 1 ]; then
-        echo $file " Errors: " $(grep -c '[\&]' $TMP_FILE_1) "Unknown words" $(grep -c '[\#]' $TMP_FILE_1) >> report_texspell
+        echo $FILE " Errors: " $(grep -c '[\&]' $TMP_FILE_1) "Unknown words" $(grep -c '[\#]' $TMP_FILE_1) >> $REPORT_FILE
         ERRORS=$(($ERRORS +  $(grep -c '[\&]' $TMP_FILE_1)))
         UNKNOWN_WORDS=$(($UNKNOWN_WORDS + $(grep -c '[\#]' $TMP_FILE_1)))
         FILES=$(($FILES+1))
@@ -218,16 +256,16 @@ if [ $SPELL -eq 1 ]; then
 
   done
   if [ $REPORT -eq 1 ]; then
-    sed -i "1 aEnd    $(date)"  report_texspell
-    echo "" >> report_texspell
-    echo "Errors files " $FILES >> report_texspell
-    echo "Empty files: " $EMPTY_FILES >> report_texspell
-    echo "Errors: " $ERRORS >> report_texspell
-    echo "Unknown words: " $UNKNOWN_WORDS >> report_texspell
+    sed -i "1 aEnd    $(date)"  $REPORT_FILE
+    echo "" >> $REPORT_FILE
+    echo "Errors files " $FILES >> $REPORT_FILE
+    echo "Empty files: " $EMPTY_FILES >> $REPORT_FILE
+    echo "Errors: " $ERRORS >> $REPORT_FILE
+    echo "Unknown words: " $UNKNOWN_WORDS >> $REPORT_FILE
     if [ $VERBOSE -ge 1 ]; then
       echo ""
       echo ""
-      cat report_texspell
+      cat $REPORT_FILE
     fi
   fi
 fi
